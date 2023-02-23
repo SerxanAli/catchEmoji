c015797681ef1a1c4d2ddafba6d601fa61960bad
SerxanAli <aliyev.s.v1990@gmail.com>
2023-02-23 12:56:12 +0400

    Two

--- catchEmoji/ViewController.swift
+++ catchEmoji/ViewController.swift
@@ -5,17 +5,24 @@ class ViewController: UIViewController {
     
     // birinci Ders
     
+    let timer = Timer()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(haraket), userInfo: nil, repeats: true)
         
         
         
     }
     
     
-    
+    @objc func haraket(){
+        
+        // 
+        
+    }
 
 
 }
